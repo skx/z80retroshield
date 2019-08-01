dd6550e3e08e00789961eded8c490c5f843e6b17
Steve Kemp <steve@steve.org.uk>
2019-08-01 20:40:38 +0300

    Updated comments and callback name.
    Also added link to source.

--- examples/uc/uc.ino
+++ examples/uc/uc.ino
@@ -1,6 +1,6 @@
 //
 // This is a very basic program which reads input from the serial-console,
-// converts it to upper-cases, and outputs it again.
+// converts it to upper-cases, and outputs it.
 //
 // Steve
 //
@@ -11,13 +11,15 @@
 //
 // Our program.
 //
-unsigned char rom[32] =
+// The source code is located in `uc.z80`.
+//
+unsigned char memory[32] =
 {
     0x3e, 0x3e, 0xd3, 0x01, 0xdb, 0x01, 0xfe, 0xff, 0xca, 0x1b, 0x00, 0xfe,
     0x61, 0xda, 0x17, 0x00, 0xfe, 0x7a, 0xd2, 0x17, 0x00, 0xd6, 0x20, 0xd3,
     0x01, 0x18, 0xe9, 0x76
 };
-int rom_len = sizeof(rom) / sizeof(rom[0]);
+int memory_len = sizeof(memory) / sizeof(memory[0]);
 
 
 //
@@ -29,12 +31,9 @@ Z80RetroShield cpu;
 //
 // RAM I/O function handler.
 //
-char ram_read(int address)
+char memory_read(int address)
 {
-    if (address >= rom_len)
-        address = 0;
-
-    return (rom[address]) ;
+    return (memory[address]) ;
 }
 
 
@@ -83,13 +82,15 @@ void setup()
     //
     // Setup callbacks.
     //
-    // We have to setup a RAM-read callback, otherwise the program
+    // We must setup a RAM-read callback, otherwise the program
     // won't be fetched from RAM and executed.
     //
-    // Then we setup a callback to be executed every time an IN/OUT
+    cpu.set_memory_read(memory_read);
+
+    //
+    // Now we setup a callback to be executed every time an IN/OUT
     // instruction is encountered.
     //
-    cpu.set_ram_read(ram_read);
     cpu.set_io_read(io_read);
     cpu.set_io_write(io_write);
 
