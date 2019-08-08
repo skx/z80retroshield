5d8c9190141b9b9cbd75144b99257b06d9ec5216
Steve Kemp <steve@steve.org.uk>
2019-08-08 21:11:09 +0300

    Added speed test

---
+++ examples/speed_test/speed_test.ino
@@ -0,0 +1,81 @@
+//
+// This example just tests how fast we're running our CPU.
+//
+// Steve
+//
+
+
+#include <z80retroshield.h>
+
+
+//
+// Our helper
+//
+Z80RetroShield cpu;
+
+
+//
+// RAM I/O function handler.
+//
+char memory_read(int address)
+{
+    //
+    // All reads will return NOP (0x00)
+    //
+    return 0x00;
+}
+
+//
+// Setup routine: Called once.
+//
+void setup()
+{
+    Serial.begin(115200);
+
+
+    //
+    // Setup callbacks.
+    //
+    // We must setup a RAM-read callback, otherwise the program
+    // won't be fetched from RAM and executed.
+    //
+    cpu.set_memory_read(memory_read);
+
+
+    //
+    // Configured.
+    //
+    Serial.println("Z80 configured; launching program.");
+}
+
+
+//
+// Loop function: Called forever.
+//
+void loop()
+{
+    //
+    // Static variables.
+    //
+    static long lastMillis = 0;
+    static int cycles = 0;
+
+    long currentMillis = millis();
+
+
+    //
+    // Step the CPU, and count it.
+    //
+    cpu.Tick();
+    cycles++;
+
+
+    if (currentMillis - lastMillis > 1000)
+    {
+        Serial.print("Cycles per second second:");
+        Serial.println(cycles);
+
+        lastMillis = currentMillis;
+        cycles = 0;
+    }
+}
